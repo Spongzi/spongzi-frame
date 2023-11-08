be0db6f7e7b027bef121c7b90da58422f1cc09a5
spongzi <spongzicode@163.com>
2023-11-08 08:24:59 +0800

    feat：集成redis

--- spongzi-user/src/main/java/com/spongzi/user/controller/TestController.java
+++ spongzi-user/src/main/java/com/spongzi/user/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.spongzi.user.controller;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * 测试控制器
@@ -11,8 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+    @GetMapping("/testRedis")
+    public void testRedis(){
+        stringRedisTemplate.opsForValue().set("name", "zhangsan");
     }
 }
